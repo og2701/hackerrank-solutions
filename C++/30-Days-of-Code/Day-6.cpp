190cf2232af35b7a7fcd250b022ee82c5f15dbc7
og2701 <67282414+og2701@users.noreply.github.com>
2021-01-24 17:09:24 +0000

    Create Day-6.cpp

---
+++ C++/30-Days-of-Code/Day-6.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i=0; i<n; i++){
+        string s;
+        cin >> s;
+        
+        for(int j=0; j<s.length(); j+=2){
+            cout << s[j];
+        }
+        cout << ' ';
+        for(int j=1; j<s.length(); j+=2){
+            cout << s[j];
+        }
+        cout << endl;
+    }
+      
+    return 0;
+}
